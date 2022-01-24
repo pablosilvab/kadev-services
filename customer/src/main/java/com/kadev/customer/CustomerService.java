1aac2ff2f7e9206083dae176385af7dd1ba5ca1c
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-01-24 00:22:34 -0300

    communication between services using rest template

--- customer/src/main/java/com/kadev/customer/CustomerService.java
+++ customer/src/main/java/com/kadev/customer/CustomerService.java
@@ -2,12 +2,14 @@ package com.kadev.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService{
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -17,6 +19,16 @@ public class CustomerService{
                 .build();
         // TODO: check if email valid
         // TODO: check if email not taken
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        // TODO: check is fraudster customer
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http:localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if (fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Fraudster!!");
+        }
+        // TODO: send notification
     }
 }
---
+++ customer/src/main/java/com/kadev/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.kadev.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
