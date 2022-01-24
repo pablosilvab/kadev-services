37853064c92dfb2857e4f6cf4b4682d7c3233d23
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-01-24 00:31:46 -0300

    fix url and add log

--- fraud/src/main/java/com/kadev/fraud/FraudController.java
+++ fraud/src/main/java/com/kadev/fraud/FraudController.java
@@ -1,8 +1,10 @@
 package com.kadev.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
@@ -13,6 +15,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("Fraud check request for customerId {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
