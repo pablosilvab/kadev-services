07be6d63d4566eba912622624f2c1cc9f17f9054
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-01-31 21:49:09 -0300

    update docker compose with notification, fraud and customer services

--- notification/src/main/java/com/kadev/notification/NotificationApplication.java
+++ notification/src/main/java/com/kadev/notification/NotificationApplication.java
@@ -2,31 +2,18 @@ package com.kadev.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(
         scanBasePackages = {
-                    "com.kadev.notification",
+                "com.kadev.notification",
                 "com.kadev.amqp"
         }
 )
+@EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
-
-    /*
-    @Bean
-    CommandLineRunner commandLineRunner(
-            RabbitMQMessageProducer producer,
-            NotificationConfig notificationConfig
-    ){
-        return args -> {
-            producer.publish(
-                    "foo",
-                    notificationConfig.getInternalExchange(),
-                    notificationConfig.getInternalNotificationRoutingKey());
-        };
-    }*/
-
 
 }
